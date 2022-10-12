4f45495fde5057da994434ff2bac4d461b49f4ff
Cheng JIANG <alex_cj96@foxmail.com>
2022-10-12 21:21:11 +0800

    fix: lint
    Signed-off-by: Cheng JIANG <alex_cj96@foxmail.com>

--- tasks/deployments/testnet/steps/02_addressProvider.ts
+++ tasks/deployments/testnet/steps/02_addressProvider.ts
@@ -1,5 +1,4 @@
 import {deployPoolAddressesProvider} from "../../../../helpers/contracts-deployments";
-import {isProxyAddress} from "../../../../helpers/contracts-getters";
 import {getParaSpaceAdmins} from "../../../../helpers/contracts-helpers";
 import {waitForTx} from "../../../../helpers/misc-utils";
 import ParaSpaceConfig from "../../../../market-config";
