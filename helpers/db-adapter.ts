6f4a5ca8c495526d44a49f1808e4d9b3db73548d
alannotnerd <alan1995wang@outlook.com>
2022-10-17 16:10:52 +0800

    Fix type issues (#13)

--- helpers/db-adapter.ts
+++ helpers/db-adapter.ts
@@ -1,3 +1,4 @@
+import type {AdapterSync} from "lowdb";
 import {default as FileAdapter} from "lowdb/adapters/FileSync";
 
 class MemoryAdapter {
@@ -10,5 +11,5 @@ class MemoryAdapter {
   }
 }
 
-export const getAdapter = (file: string): any =>
-  file === ":memory:" ? new MemoryAdapter() : new FileAdapter(file);
+export const getAdapter = (file: string): AdapterSync =>
+  (file === ":memory:" ? new MemoryAdapter() : new FileAdapter(file)) as any;
