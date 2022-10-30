e3a1e182cb68e337825df29f254cc900d835a393
Cheng JIANG <alex_cj96@foxmail.com>
2022-10-31 00:27:54 +0800

    feat: add mainnet deployment config
    Signed-off-by: Cheng JIANG <alex_cj96@foxmail.com>

--- market-config/index.ts
+++ market-config/index.ts
@@ -101,15 +101,10 @@ export const HardhatParaSpaceConfig: IParaSpaceConfiguration = {
 export const GoerliParaSpaceConfig: IParaSpaceConfiguration = {
   // BASIC INFO
   ...CommonConfig,
-  ParaSpaceTeam: "0x69C33aB569816F1D564a420490AbB894a44071Fb",
+  ParaSpaceTeam: "0x03D10cda221C2faA9F5CA22087654011ceE1802D",
   Treasury: "0x03D10cda221C2faA9F5CA22087654011ceE1802D",
-  Tokens: {
-    // WETH: "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6",
-    // USDC: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
-  },
-  BendDAO: {
-    Oracle: "0x7c2a19e54e48718f6c60908a9cff3396e4ea1eba",
-  },
+  Tokens: {},
+  BendDAO: {},
   Uniswap: {
     V2Factory: "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f",
     V2Router: "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
@@ -153,8 +148,8 @@ export const MainnetParaSpaceConfig: IParaSpaceConfiguration = {
   ParaSpaceTeam: "0x69C33aB569816F1D564a420490AbB894a44071Fb",
   Treasury: "0x03D10cda221C2faA9F5CA22087654011ceE1802D",
   Tokens: {
-    // WETH: "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6",
-    // USDC: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+    WETH: "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6",
+    USDC: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
   },
   BendDAO: {
     Oracle: "0x7c2a19e54e48718f6c60908a9cff3396e4ea1eba",
@@ -164,9 +159,6 @@ export const MainnetParaSpaceConfig: IParaSpaceConfiguration = {
     V2Router: "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
     V3Factory: "0x1F98431c8aD98523631AE4a59f267346ea31F984",
     V3NFTPositionManager: "0xC36442b4a4522E871399CD717aBDD847Ab11FE88",
-    // MAINNET
-    // V2Factory: "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f",
-    // V2Router: "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"
   },
   Marketplace: {
     Seaport: "0x00000000006c3852cbEf3e08E8dF289169EdE581",
@@ -177,7 +169,6 @@ export const MainnetParaSpaceConfig: IParaSpaceConfiguration = {
     USDC: "0x986b5E1e1755e3C2440e960477f25201B0a8bbD4",
     USDT: "0xEe9F2375b4bdF6387aa8265dD4FB8F16512A1d46",
     WBTC: "0xdeb288F737066589598e9214E782fa5A8eD689e8",
-    STETH: "0x86392dC19c0b719886221c78AB11eb8Cf5c52812",
     APE: "0xc7de7f4d4C9c991fF62a07D18b3E31e349833A18",
   },
   // RESERVE ASSETS - CONFIG, ASSETS, BORROW RATES,
@@ -188,20 +179,14 @@ export const MainnetParaSpaceConfig: IParaSpaceConfiguration = {
     WETH: strategyWETH,
     APE: strategyAPE,
     WBTC: strategyWBTC,
-    stETH: strategySTETH,
-    aWETH: strategyAWETH,
-    cETH: strategyCETH,
-    PUNK: strategyPUNK,
     BAYC: strategyBAYC,
     MAYC: strategyMAYC,
     DOODLE: strategyDOODLES,
-    WPUNKS: strategyWPUNKS,
     MOONBIRD: strategyMoonbird,
     MEEBITS: strategyMeebits,
     AZUKI: strategyAzuki,
     OTHR: strategyOthr,
     CLONEX: strategyClonex,
-    UniswapV3: strategyUniswapV3,
   },
 };
 
