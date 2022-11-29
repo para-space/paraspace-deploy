e6ca9885431edcf8bf7eb2b7694a039a02fdab68
GopherJ <alex_cj96@foxmail.com>
2022-11-30 01:52:57 +0800

    chore: fix lint
    Signed-off-by: GopherJ <alex_cj96@foxmail.com>

--- tasks/deployments/dev/1.ad-hoc.ts
+++ tasks/deployments/dev/1.ad-hoc.ts
@@ -7,24 +7,13 @@ import {
 import {DRE} from "../../../helpers/misc-utils";
 import * as envs from "../../../helpers/hardhat-constants";
 import {accounts} from "../../../wallets";
-import {MarketRegistry__factory} from "../../../../types";
 // import {waitForTx} from "../../../helpers/misc-utils";
 
 const adHoc = async () => {
   await DRE.run("set-DRE");
   console.time("ad-hoc");
-  const signer = await getFirstSigner();
 
-  // const marketRegistry = MarketRegistry__factory.connect(
-  //   "0x3a574baC669F3B1CB54b92cCBAefbAFd07054d96",
-  //   signer
-  // );
-
-  // for (let i = 0; i < 12; i++) {
-  //   console.log(await marketRegistry.markets(i.toString()));
-  // }
-
-  // console.log(accounts);
+  console.log(accounts);
 
   // await waitForTx(await signer.sendTransaction({
   //   to: "0x018281853eCC543Aa251732e8FDaa7323247eBeB",
@@ -32,13 +21,13 @@ const adHoc = async () => {
   //   ...envs.GLOBAL_OVERRIDES
   // }))
 
-  // console.log(await DRE.ethers.provider.getNetwork());
-  // console.log(await (await getFirstSigner()).getAddress());
-  // console.log(await getParaSpaceAdmins());
-  // console.log(
-  //   await Promise.all((await getEthersSigners()).map((x) => x.getAddress()))
-  // );
-  // console.log(envs);
+  console.log(await DRE.ethers.provider.getNetwork());
+  console.log(await (await getFirstSigner()).getAddress());
+  console.log(await getParaSpaceAdmins());
+  console.log(
+    await Promise.all((await getEthersSigners()).map((x) => x.getAddress()))
+  );
+  console.log(envs);
 
   // Your main logic
   console.timeEnd("ad-hoc");
