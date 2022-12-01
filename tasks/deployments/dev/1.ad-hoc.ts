cd6ef6b559641db6614c15b5e1ebf9513e7b19b3
GopherJ <alex_cj96@foxmail.com>
2022-12-01 15:17:41 +0800

    feat: more checkes
    Signed-off-by: GopherJ <alex_cj96@foxmail.com>

--- tasks/deployments/dev/1.ad-hoc.ts
+++ tasks/deployments/dev/1.ad-hoc.ts
@@ -3,16 +3,18 @@ import {
   getACLManager,
   getConduit,
   getConduitController,
+  getMintableERC20,
   getNFTFloorOracle,
+  getParaSpaceOracle,
   getPausableZoneController,
   getPoolAddressesProvider,
   getPoolAddressesProviderRegistry,
   getProtocolDataProvider,
   getUiPoolDataProvider,
-  getWETHGateway,
   getWETHGatewayProxy,
 } from "../../../helpers/contracts-getters";
 import {
+  getEthersSignersAddresses,
   getParaSpaceAdmins,
   impersonateAddress,
 } from "../../../helpers/contracts-helpers";
@@ -21,6 +23,8 @@ import {DRE, waitForTx} from "../../../helpers/misc-utils";
 const adHoc = async () => {
   await DRE.run("set-DRE");
   console.time("ad-hoc");
+  console.log(await getEthersSignersAddresses());
+  console.log(await getParaSpaceAdmins());
 
   console.log(await getParaSpaceAdmins());
   const poolAddressesProvider = await getPoolAddressesProvider();
@@ -114,6 +118,28 @@ const adHoc = async () => {
     (await addressesProviderRegistry.owner()) ==
       "0x17816E9A858b161c3E37016D139cf618056CaCD4"
   );
+  await Promise.all(
+    [
+      "0x17816E9A858b161c3E37016D139cf618056CaCD4",
+      "0x69FAD68De47D5666Ad668C7D682dDb8FD6322949",
+      "0xD65Fee206a4ea89eBBcF4694E745C597AB6F8325",
+      "0x755C1bd877788739dD002B98B093c4852AbfA6c4",
+      "0x3A6c796edffc057d789F7d4ffAd438B1D48f3075",
+      "0x2f2d07d60ea7330DD2314f4413CCbB2dC25276EF",
+      "0x001e2bcC5c1BfC3131d33Ba074B12c2F1237FB04",
+    ].map(async (x) => await aclManager.isEmergencyAdmin(x))
+  );
+
+  const WETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+  const USDC = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
+  const APE = "0x4d224452801ACEd8B2F0aebE155379bb5D594381";
+  const USDT = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
+
+  const paraspaceOracle = await getParaSpaceOracle();
+  console.log("USDC: ", (await paraspaceOracle.getAssetPrice(USDC)).toString());
+  console.log("USDT: ", (await paraspaceOracle.getAssetPrice(USDT)).toString());
+  console.log("WETH: ", (await paraspaceOracle.getAssetPrice(WETH)).toString());
+  console.log("APE: ", (await paraspaceOracle.getAssetPrice(APE)).toString());
 
   // Your main logic
   console.timeEnd("ad-hoc");
